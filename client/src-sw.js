6672d4e6b6c836e2b1f46daf765f6e94ba32790d
Madeline K <madkovanda@gmail.com>
2023-11-17 13:36:44 -0700

    updae

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,7 +7,7 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
-//self.__WB_MANIFEST = (self.__WB_MANIFEST || []).concat(self.__precacheManifest || []);
+//self.__WB_MANIFEST = (self.__WB_MANIFEST || []).concat(self.__precacheManifest || []); 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -60,3 +60,54 @@ registerRoute(
 registerRoute();
 
 
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
