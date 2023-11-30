c01a4e5c04800db825000bee54203634f04d6924
Madeline K <madkovanda@gmail.com>
2023-11-29 23:17:51 -0700

    update service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -52,3 +52,14 @@ const urlsToCache = [
   '/styles/main.css',
   '/script/main.js'
 ];
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/src-sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
