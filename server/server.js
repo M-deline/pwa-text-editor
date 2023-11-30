642cde3505f8897dd46059ff238ac8363d023014
Madeline K <madkovanda@gmail.com>
2023-11-29 23:38:58 -0700

    edit heroku issues

--- server/server.js
+++ server/server.js
@@ -3,15 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static('../client/dist'));
-// app.use('/src-sw.js', express.static(path.join(__dirname, '../../client/dist/src-sw.js')));
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
 app.use('/src-sw.js', express.static(path.join(__dirname, '../client/src-sw.js')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require('./routes/htmlRoutes.js')(app);
-// require('./routes/htmlRoutes')(app);
-// express.static();
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
