52bccbd600f5b13b4c304fc9cb417bf2c7ceafed
Madeline K <madkovanda@gmail.com>
2023-11-30 00:08:24 -0700

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/src-sw.js', express.static(path.join(__dirname, '../client/src-sw.js')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-require('./routes/htmlRoutes.js')(app);
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+git remote -v
